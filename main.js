487af8f8a2e1189bb3578cee117aacca90170231
vivaangovindugari <97267116+vivaangovindugari@users.noreply.github.com>
2022-11-20 18:45:20 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+ 
+function setup(){
+    var canvas =  createCanvas(700,600);
+    canvas.parent('canvas');
+    
+    video = createCapture(VIDEO);
+    video.size(700, 600);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    }
+    
+    function modelLoaded() {
+      console.log('PoseNet Is Initialized');
+    }
+    
+    function gotPoses(results)
+    {
+      if(results.length > 0)
+      {
+    
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        scoreRightWrist =  results[0].pose.keypoints[10].score;
+        console.log(scoreRightWrist);
+      }
+    }
